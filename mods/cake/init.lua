e48f5b968123bc61b9468def33c84f3def580b14
Elkien3 <Elkien3@users.noreply.github.com>
2017-09-23 21:34:40 -0500

    Edit cake to support hud
    They will now fill your hunder instead of heal.

--- mods/cake/init.lua
+++ mods/cake/init.lua
@@ -47,8 +47,12 @@ for i, size in ipairs(sizes) do
 			}
 		},
 		on_rightclick = function(pos, node, clicker)
-			clicker:set_hp(clicker:get_hp() + 1)
-			
+		--	clicker:set_hp(clicker:get_hp() + 1)
+			local name = clicker:get_player_name()
+			local h = tonumber(hud.hunger[name])
+			h = h + 4
+			hud.hunger[name] = h
+			hud.set_hunger(clicker)
 			if i < #sizes then
 				minetest.swap_node(pos, {name="cake:cake_"..i})
 			else
