88430ec55ef29d0286932739816bdd24baafa600
Elkien3 <Elkien3@users.noreply.github.com>
2017-10-04 11:15:04 -0500

    Updated Books Mod
    I need the page system added to my books, Hopefully books that are already made with work alright with this.

--- mods/default/craftitems.lua
+++ mods/default/craftitems.lua
@@ -11,13 +11,41 @@ minetest.register_craftitem("default:paper", {
 	inventory_image = "default_paper.png",
 })
 
-local function book_on_use(itemstack, user, pointed_thing)
+local lpp = 14 -- Lines per book's page
+local function book_on_use(itemstack, user)
 	local player_name = user:get_player_name()
-	local data = minetest.deserialize(itemstack:get_metadata())
+	local meta = itemstack:get_meta()
 	local title, text, owner = "", "", player_name
-	if data then
-		title, text, owner = data.title, data.text, data.owner
+	local page, page_max, lines, string = 1, 1, {}, ""
+
+	-- Backwards compatibility
+	local old_data = minetest.deserialize(itemstack:get_metadata())
+	if old_data then
+		meta:from_table({ fields = old_data })
 	end
+
+	local data = meta:to_table().fields
+
+	if data.owner then
+		title = data.title
+		text = data.text
+		owner = data.owner
+
+		for str in (text .. "\n"):gmatch("([^\n]*)[\n]") do
+			lines[#lines+1] = str
+		end
+
+		if data.page then
+			page = data.page
+			page_max = data.page_max
+
+			for i = ((lpp * page) - lpp) + 1, lpp * page do
+				if not lines[i] then break end
+				string = string .. lines[i] .. "\n"
+			end
+		end
+	end
+
 	local formspec
 	if owner == player_name then
 		formspec = "size[8,8]" .. default.gui_bg ..
@@ -31,61 +59,103 @@ local function book_on_use(itemstack, user, pointed_thing)
 		formspec = "size[8,8]" .. default.gui_bg ..
 			default.gui_bg_img ..
 			"label[0.5,0.5;by " .. owner .. "]" ..
-			"label[0.5,0;" .. minetest.formspec_escape(title) .. "]" ..
-			"textarea[0.5,1.5;7.5,7;text;;" ..
-				minetest.formspec_escape(text) .. "]"
+			"tablecolumns[color;text]" ..
+			"tableoptions[background=#00000000;highlight=#00000000;border=false]" ..
+			"table[0.4,0;7,0.5;title;#FFFF00," .. minetest.formspec_escape(title) .. "]" ..
+			"textarea[0.5,1.5;7.5,7;;" ..
+				minetest.formspec_escape(string ~= "" and string or text) .. ";]" ..
+			"button[2.4,7.6;0.8,0.8;book_prev;<]" ..
+			"label[3.2,7.7;Page " .. page .. " of " .. page_max .. "]" ..
+			"button[4.9,7.6;0.8,0.8;book_next;>]"
 	end
-	minetest.show_formspec(user:get_player_name(), "default:book", formspec)
+
+	minetest.show_formspec(player_name, "default:book", formspec)
+	return itemstack
 end
 
-minetest.register_on_player_receive_fields(function(player, form_name, fields)
-	if form_name ~= "default:book" or not fields.save or
-			fields.title == "" or fields.text == "" then
-		return
-	end
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+	if formname ~= "default:book" then return end
 	local inv = player:get_inventory()
 	local stack = player:get_wielded_item()
-	local new_stack, data
-	if stack:get_name() ~= "default:book_written" then
-		local count = stack:get_count()
-		if count == 1 then
-			stack:set_name("default:book_written")
+
+	if fields.save and fields.title ~= "" and fields.text ~= "" then
+		local new_stack, data
+		if stack:get_name() ~= "default:book_written" then
+			local count = stack:get_count()
+			if count == 1 then
+				stack:set_name("default:book_written")
+			else
+				stack:set_count(count - 1)
+				new_stack = ItemStack("default:book_written")
+			end
 		else
-			stack:set_count(count - 1)
-			new_stack = ItemStack("default:book_written")
+			data = stack:get_meta():to_table().fields
 		end
-	else
-		data = minetest.deserialize(stack:get_metadata())
-	end
-	if not data then data = {} end
-	data.title = fields.title
-	data.text = fields.text
-	data.owner = player:get_player_name()
-	local data_str = minetest.serialize(data)
-	if new_stack then
-		new_stack:set_metadata(data_str)
-		if inv:room_for_item("main", new_stack) then
-			inv:add_item("main", new_stack)
+
+		if data and data.owner and data.owner ~= player:get_player_name() then
+			return
+		end
+
+		if not data then data = {} end
+		data.title = fields.title
+		data.owner = player:get_player_name()
+		data.description = "\""..fields.title.."\" by "..data.owner
+		data.text = fields.text
+		data.text_len = #data.text
+		data.page = 1
+		data.page_max = math.ceil((#data.text:gsub("[^\n]", "") + 1) / lpp)
+
+		if new_stack then
+			new_stack:get_meta():from_table({ fields = data })
+			if inv:room_for_item("main", new_stack) then
+				inv:add_item("main", new_stack)
+			else
+				minetest.add_item(player:getpos(), new_stack)
+			end
 		else
-			minetest.add_item(player:getpos(), new_stack)
+			stack:get_meta():from_table({ fields = data })
 		end
-	else
-		stack:set_metadata(data_str)
+
+	elseif fields.book_next or fields.book_prev then
+		local data = stack:get_meta():to_table().fields
+		if not data or not data.page then
+			return
+		end
+
+		data.page = tonumber(data.page)
+		data.page_max = tonumber(data.page_max)
+
+		if fields.book_next then
+			data.page = data.page + 1
+			if data.page > data.page_max then
+				data.page = 1
+			end
+		else
+			data.page = data.page - 1
+			if data.page == 0 then
+				data.page = data.page_max
+			end
+		end
+
+		stack:get_meta():from_table({fields = data})
+		stack = book_on_use(stack, player)
 	end
+
+	-- Update stack
 	player:set_wielded_item(stack)
 end)
 
 minetest.register_craftitem("default:book", {
 	description = "Book",
 	inventory_image = "default_book.png",
-	groups = {book = 1},
+	groups = {book = 1, flammable = 3},
 	on_use = book_on_use,
 })
 
 minetest.register_craftitem("default:book_written", {
 	description = "Book With Text",
 	inventory_image = "default_book_written.png",
-	groups = {book = 1, not_in_creative_inventory = 1},
+	groups = {book = 1, not_in_creative_inventory = 1, flammable = 3},
 	stack_max = 1,
 	on_use = book_on_use,
 })
@@ -101,7 +171,6 @@ minetest.register_on_craft(function(itemstack, player, old_craft_grid, craft_inv
 		return
 	end
 
-	local copy = ItemStack("default:book_written")
 	local original
 	local index
 	for i = 1, player:get_inventory():get_size("craft") do
@@ -113,9 +182,9 @@ minetest.register_on_craft(function(itemstack, player, old_craft_grid, craft_inv
 	if not original then
 		return
 	end
-	local copymeta = original:get_metadata()
+	local copymeta = original:get_meta():to_table()
 	-- copy of the book held by player's mouse cursor
-	itemstack:set_metadata(copymeta)
+	itemstack:get_meta():from_table(copymeta)
 	-- put the book with metadata back in the craft grid
 	craft_inv:set_stack("craft", index, original)
 end)
