5154c8230c3d4d345dd86641cb8db0f670805fc3
Elkien3 <Elkien3@users.noreply.github.com>
2018-01-26 08:09:49 -0600

    make cactus damage

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -885,6 +885,7 @@ minetest.register_node("default:cactus", {
 		"default_cactus_side.png"},
 	paramtype2 = "facedir",
 	groups = {snappy = 1, choppy = 3, flammable = 2},
+	damage_per_second = 4,
 	sounds = default.node_sound_wood_defaults(),
 	on_place = minetest.rotate_node,
 })
