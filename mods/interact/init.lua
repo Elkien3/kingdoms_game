65fa9de5146df93555b774a48273ddebda19bcc3
Elkien3 <Elkien3@users.noreply.github.com>
2018-05-20 21:08:47 -0500

    update interact mod
    it should be able to support multiple translations fairly easily now.

--- mods/interact/init.lua
+++ mods/interact/init.lua
@@ -1,5 +1,7 @@
+rule_table = {}
+rule_language = {}
+dofile(minetest.get_modpath("interact") .. "/rules-english.lua") --I put the rules in their own file so that they don't get lost/overlooked!
 dofile(minetest.get_modpath("interact") .. "/config.lua")
-dofile(minetest.get_modpath("interact") .. "/rules.lua") --I put the rules in their own file so that they don't get lost/overlooked!
 
 local rule1 = 0
 local rule2 = 0
@@ -7,59 +9,72 @@ local rule3 = 0
 local rule4 = 0
 local multi = 0
 
-local function make_formspec(player)
+local all_languages = interact.default_language
+for k in pairs(rule_table) do
+	if k ~= interact.default_language then
+		all_languages = all_languages..", "..k
+	end
+end
+
+local function make_formspec(player, language)
+	if not language then language = interact.default_language end
 	local name = player:get_player_name()
 	local size = { "size[10,4]" }
-	table.insert(size, "label[0.5,0.5;" ..interact.s1_header.. "]")
-	table.insert(size, "label[0.5,1.5;" ..interact.s1_l2.. "]")
-	table.insert(size, "label[0.5,2;" ..interact.s1_l3.. "]")
-	table.insert(size, "button_exit[5.5,3.4;2,0.5;no;" ..interact.s1_b1.. "]")
-	table.insert(size, "button[7.5,3.4;2,0.5;yes;" ..interact.s1_b2.. "]")
+	table.insert(size, "label[1,0.5;List of Languages (eg: /rules english)]")
+	table.insert(size, "label[1,1;"..all_languages.."]")
+	table.insert(size, "label[0.5,0;" ..rule_table[language].s1_header.. "]")
+	table.insert(size, "label[0.5,3.5;" ..rule_table[language].s1_l2.. "]")
+	table.insert(size, "label[0.5,2;" ..rule_table[language].s1_l3.. "]")
+	table.insert(size, "button_exit[5.5,3.4;2,0.5;no;" ..rule_table[language].s1_b1.. "]")
+	table.insert(size, "button[7.5,3.4;2,0.5;yes;" ..rule_table[language].s1_b2.. "]")
 	return table.concat(size)
 end
 
-local function make_formspec2(player)
+local function make_formspec2(player, language)
+	if not language then language = interact.default_language end
 	local name = player:get_player_name()
 	local size = { "size[10,4]" }
-	table.insert(size, "label[0.5,0.5;" ..interact.s2_l1.. "]")
-	table.insert(size, "label[0.5,1;" ..interact.s2_l2.. "]")
-	table.insert(size, "button_exit[2.5,3.4;3.5,0.5;interact;" ..interact.s2_b1.. "]")
-	table.insert(size, "button_exit[6.4,3.4;3.6,0.5;visit;" ..interact.s2_b2.. "]")
+	table.insert(size, "label[0.5,0.5;" ..rule_table[language].s2_l1.. "]")
+	table.insert(size, "label[0.5,1;" ..rule_table[language].s2_l2.. "]")
+	table.insert(size, "button_exit[2.5,3.4;3.5,0.5;interact;" ..rule_table[language].s2_b1.. "]")
+	table.insert(size, "button_exit[6.4,3.4;3.6,0.5;visit;" ..rule_table[language].s2_b2.. "]")
 	return table.concat(size)
 end
 
-local function make_formspec3(player)
+local function make_formspec3(player, language)
+	if not language then language = interact.default_language end
 	local size = { "size[10,8]" }
-	table.insert(size, "textarea[0.5,0.5;9.5,7.5;TOS;" ..interact.s3_header.. ";" ..interact.rules.. "]")
-	table.insert(size, "button[5.5,7.4;2,0.5;decline;" ..interact.s3_b2.. "]")
-	table.insert(size, "button_exit[7.5,7.4;2,0.5;accept;" ..interact.s3_b1.. "]")
+	table.insert(size, "textarea[0.5,0.5;9.5,7.5;TOS;" ..rule_table[language].s3_header.. ";" ..rule_table[language].rules.. "]")
+	table.insert(size, "button[5.5,7.4;2,0.5;decline;" ..rule_table[language].s3_b2.. "]")
+	table.insert(size, "button_exit[7.5,7.4;2,0.5;accept;" ..rule_table[language].s3_b1.. "]")
 	return table.concat(size)
 end
 
-local function make_formspec4(player)
+local function make_formspec4(player, language)
+	if not language then language = interact.default_language end
 	local name = player:get_player_name()
 	local size = { "size[10,9]" }
 	if interact.s4_to_rules_button == true then
-		table.insert(size, "button_exit[7.75,0.25;2.1,0.1;rules;" ..interact.s4_to_rules.. "]")
+		table.insert(size, "button_exit[7.75,0.25;2.1,0.1;rules;" ..rule_table[language].s4_to_rules.. "]")
 	end
-	table.insert(size, "label[0.25,0;" ..interact.s4_header.."]")
-	table.insert(size, "label[0.5,0.5;" ..interact.s4_question1.."]")
-	table.insert(size, "checkbox[0.25,1;rule1_true;" ..interact.s4_question1_true.."]")
-	table.insert(size, "checkbox[4,1;rule1_false;" ..interact.s4_question1_false.. "]")
-	table.insert(size, "label[0.5,2;" ..interact.s4_question2.. "]")
-	table.insert(size, "checkbox[0.25,2.5;rule2_true;" ..interact.s4_question2_true.. "]")
-	table.insert(size, "checkbox[4,2.5;rule2_false;" ..interact.s4_question2_false.. "]")
-	table.insert(size, "label[0.5,3.5;" ..interact.s4_question3.. "]")
-	table.insert(size, "checkbox[0.25,4;rule3_true;" ..interact.s4_question3_true.. "]")
-	table.insert(size, "checkbox[4,4;rule3_false;" ..interact.s4_question3_false.. "]")
-	table.insert(size, "label[0.5,5;" ..interact.s4_question4.. "]")
-	table.insert(size, "checkbox[0.25,5.5;rule4_true;" ..interact.s4_question4_true.. "]")
-	table.insert(size, "checkbox[4,5.5;rule4_false;" ..interact.s4_question4_false.."]")
-	table.insert(size, "label[0.5,6.5;" ..interact.s4_multi_question.. "]")
-	table.insert(size, "checkbox[4.75,6.25;multi_choice1;" ..interact.s4_multi1.. "]")
-	table.insert(size, "checkbox[0.25,7;multi_choice2;" ..interact.s4_multi2.. "]")
-	table.insert(size, "checkbox[4.75,7;multi_choice3;" ..interact.s4_multi3.."]")
-	table.insert(size, "button_exit[3,8.4;3.5,0.5;submit;" ..interact.s4_submit.."]")
+	table.insert(size, "label[0.25,0;" ..rule_table[language].s4_header.."]")
+	table.insert(size, "label[0.5,0.5;" ..rule_table[language].question1.."]")
+	table.insert(size, "checkbox[0.25,1;rule1_true;" ..rule_table[language].s4_question1_true.."]")
+	table.insert(size, "checkbox[4,1;rule1_false;" ..rule_table[language].s4_question1_false.. "]")
+	table.insert(size, "label[0.5,2;" ..rule_table[language].question2.. "]")
+	table.insert(size, "checkbox[0.25,2.5;rule2_true;" ..rule_table[language].s4_question2_true.. "]")
+	table.insert(size, "checkbox[4,2.5;rule2_false;" ..rule_table[language].s4_question2_false.. "]")
+	table.insert(size, "label[0.5,3.5;" ..rule_table[language].question3.. "]")
+	table.insert(size, "checkbox[0.25,4;rule3_true;" ..rule_table[language].s4_question3_true.. "]")
+	table.insert(size, "checkbox[4,4;rule3_false;" ..rule_table[language].s4_question3_false.. "]")
+	table.insert(size, "label[0.5,5;" ..rule_table[language].question4.. "]")
+	table.insert(size, "checkbox[0.25,5.5;rule4_true;" ..rule_table[language].s4_question4_true.. "]")
+	table.insert(size, "checkbox[4,5.5;rule4_false;" ..rule_table[language].s4_question4_false.."]")
+	table.insert(size, "label[0.5,6.5;" ..rule_table[language].multiquestion.. "]")
+	table.insert(size, "checkbox[4.75,6.25;multi_choice1;" ..rule_table[language].mq_answer1.. "]")
+	table.insert(size, "checkbox[0.25,7;multi_choice2;" ..rule_table[language].mq_answer2.. "]")
+	table.insert(size, "checkbox[4.75,7;multi_choice3;" ..rule_table[language].mq_answer3.."]")
+	table.insert(size, "button_exit[3,8.4;3.5,0.5;submit;" ..rule_table[language].s4_submit.."]")
 	return table.concat(size)
 end
 
@@ -76,20 +91,21 @@ local server_formspec = "size[10,4]" ..
 minetest.register_on_player_receive_fields(function(player, formname, fields)
 	if formname ~= "interact_welcome" then return end
 	local name = player:get_player_name()
+	local language = rule_language[name] or interact.default_language
 	if fields.no then
 		if interact.screen2 == false then
 			minetest.after(1, function()
-				minetest.show_formspec(name, "interact_rules", make_formspec3(player))
+				minetest.show_formspec(name, "interact_rules", make_formspec3(player, language))
 			end)
 		else
 			minetest.after(1, function()
-				minetest.show_formspec(name, "interact_visit", make_formspec2(player))
+				minetest.show_formspec(name, "interact_visit", make_formspec2(player, language))
 			end)
 		end
 		return
 	elseif fields.yes then
 		if interact.grief_ban ~= true then
-			--minetest.kick_player(name, interact.msg_grief)
+			minetest.kick_player(name, rule_table[language].msg_grief)
 		else
 			minetest.ban_player(name)
 		end
@@ -100,13 +116,14 @@ end)
 minetest.register_on_player_receive_fields(function(player, formname, fields)
 	if formname ~= "interact_visit" then return end
 	local name = player:get_player_name()
+	local language = rule_language[name] or interact.default_language
 	if fields.interact then
 		minetest.after(1, function()
-			minetest.show_formspec(name, "interact_rules", make_formspec3(player))
+			minetest.show_formspec(name, "interact_rules", make_formspec3(player, language))
 		end)
 		return
 	elseif fields.visit then
-		minetest.chat_send_player(name, interact.visit_msg)
+		minetest.chat_send_player(name, rule_table[language].visit_msg)
 		minetest.log("action", name.. " is just visiting.")
 	return
 	end
@@ -116,11 +133,12 @@ end)
 minetest.register_on_player_receive_fields(function(player, formname, fields)
 	if formname ~= "interact_rules" then return end
 	local name = player:get_player_name()
+	local language = rule_language[name] or interact.default_language
 	if fields.accept then
 		if interact.screen4 == false then
 			if minetest.check_player_privs(name, interact.priv) then
-				minetest.chat_send_player(name, interact.interact_msg1)
-				minetest.chat_send_player(name, interact.interact_msg2)
+				minetest.chat_send_player(name, rule_table[language].interact_msg1)
+				minetest.chat_send_player(name, rule_table[language].interact_msg2)
 				local privs = minetest.get_player_privs(name)
 				privs.interact = true
 				minetest.set_player_privs(name, privs)
@@ -128,17 +146,17 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 			end
 		else
 			minetest.after(1, function()
-				minetest.show_formspec(name, "interact_quiz", make_formspec4(player))
+				minetest.show_formspec(name, "interact_quiz", make_formspec4(player, language))
 			end)
 		end
 		return
 	elseif fields.decline then
 		if interact.disagree_action == "kick" then
-			minetest.kick_player(name, interact.disagree_msg)
+			minetest.kick_player(name, rule_table[language].disagree_msg)
 		elseif interact.disagree_action == "ban" then
 			minetest.ban_player(name)
 		else
-			minetest.chat_send_player(name, interact.disagree_msg)
+			minetest.chat_send_player(name, rule_table[language].disagree_msg)
 		end
 	return
 	end
@@ -147,9 +165,10 @@ end)
 minetest.register_on_player_receive_fields(function(player, formname, fields)
 	if formname ~= "interact_quiz" then return end
 	local name = player:get_player_name()
+	local language = rule_language[name] or interact.default_language
 	if fields.rules then
 		minetest.after(1, function()
-			minetest.show_formspec(name, "interact_rules", make_formspec3(player))
+			minetest.show_formspec(name, "interact_rules", make_formspec3(player, language))
 		end)
 		return
 	end
@@ -172,8 +191,8 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 		rule4 = 0
 		multi = 0
 		if minetest.check_player_privs(name, interact.priv) then
-			minetest.chat_send_player(name, interact.interact_msg1)
-			minetest.chat_send_player(name, interact.interact_msg2)
+			minetest.chat_send_player(name, rule_table[language].interact_msg1)
+			minetest.chat_send_player(name, rule_table[language].interact_msg2)
 			local privs = minetest.get_player_privs(name)
 			privs.interact = true
 			minetest.set_player_privs(name, privs)
@@ -192,57 +211,64 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 		elseif interact.on_wrong_quiz == "reshow" then
 			minetest.chat_send_player(name, interact.quiz_try_again_msg)
 			minetest.after(1, function()
-				minetest.show_formspec(name, "interact_quiz", make_formspec4(player))
+				minetest.show_formspec(name, "interact_quiz", make_formspec4(player, language))
 			end)
 		elseif interact.on_wrong_quiz == "rules" then
 			minetest.chat_send_player(name, interact.quiz_rules_msg)
 			minetest.after(1, function()
-				minetest.show_formspec(name, "interact_rules", make_formspec3(player))
+				minetest.show_formspec(name, "interact_rules", make_formspec3(player, language))
 			end)
 		else
-			minetest.chat_send_player(name, interact.quiz_fail_msg)
+			minetest.chat_send_player(name, rule_table[language].quiz_fail_msg)
 		end
 	end
 end)
 
 minetest.register_chatcommand("rules",{
-	params = "",
+	params = "<language>",
 	description = "Shows the server rules",
 	privs = interact.priv,
 	func = function (name,params)
 	local player = minetest.get_player_by_name(name)
+	local language = rule_language[name] or interact.default_language
+	if params ~= "" and rule_table[params:lower()] then
+		language = params:lower()
+		rule_language[name] = language
+	elseif params ~= "" then
+		minetest.chat_send_player(name, "There is no translation for '"..params:lower().."', Opening rules in '"..language.."'")
+	end
 		if interact.screen1 ~= false then
 			minetest.after(1, function()
-				minetest.show_formspec(name, "interact_welcome", make_formspec(player))
+				minetest.show_formspec(name, "interact_welcome", make_formspec(player, language))
 			end)
 		elseif interact.screen2 ~= false then
 			minetest.after(1, function()
-				minetest.show_formspec(name, "interact_visit", make_formspec2(player))
+				minetest.show_formspec(name, "interact_visit", make_formspec2(player, language))
 			end)
 		else
 			minetest.after(1, function()
-				minetest.show_formspec(name, "interact_rules", make_formspec3(player))
+				minetest.show_formspec(name, "interact_rules", make_formspec3(player, language))
 			end)
 		end
 	end
 })
+
 minetest.register_on_joinplayer(function(player)
---minetest.after(60, function(player)
 	local name = player:get_player_name()
+	local language = rule_language[name] or interact.default_language
 	if not minetest.get_player_privs(name).interact then
 		if interact.screen1 ~= false then
-			minetest.show_formspec(name, "interact_welcome", make_formspec(player))
+			minetest.show_formspec(name, "interact_welcome", make_formspec(player, language))
 		elseif interact.screen2 ~= false then
-		--if interact.screen2 ~= false then
-			minetest.show_formspec(name, "interact_visit", make_formspec2(player))
+			minetest.show_formspec(name, "interact_visit", make_formspec2(player, language))
 		else
-			minetest.show_formspec(name, "interact_rules", make_formspec3(player))
+			minetest.show_formspec(name, "interact_rules", make_formspec3(player, language))
 		end
 	elseif minetest.get_player_privs(name).server and interact.configured == false then
 		minetest.show_formspec(name, "interact_no_changes_made", server_formspec)
 	end
---end, player)
 end)
+
 if not interact.configured then
 	minetest.log("warning", "Mod \"Interact\" has not been configured! Please open config.lua in its folder and configure it. See the readme of the mod for more details.")
 end
